cabcd2a6feb3863047edbe101ff51bf4983d3e79
Muskan-codebase <muskanmjvr@gmail.com>
2026-07-23 15:07:29 +0530

    updated firebase initialization file

--- backend/src/app/config/firebase.ts
+++ backend/src/app/config/firebase.ts
@@ -1,14 +1,19 @@
-//This File intializes/Configures Backend with Firebase 🔥
+// This file initializes/configures Backend with Firebase 🔥
 
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { ServiceAccount } from "firebase-admin";
-import serviceAccount from "./firebase/serviceAccountKey.json";
+
+const serviceAccount: ServiceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+};
 
 const app =
-  getApps().length > 0
-    ? getApps()[0]
-    : initializeApp({
-        credential: cert(serviceAccount as ServiceAccount),
-      });
+    getApps().length > 0
+        ? getApps()[0]
+        : initializeApp({
+            credential: cert(serviceAccount),
+        });
 
 export default app;
