c7794c0942623cf9def13e4fa837b9caab69a0e0
Muskan-codebase <muskanmjvr@gmail.com>
2026-07-23 15:07:50 +0530

    updated the dist folder with latest changes

--- backend/dist/src/app/config/firebase.js
+++ backend/dist/src/app/config/firebase.js
@@ -1,14 +1,16 @@
 "use strict";
-//This File intializes/Configures Backend with Firebase 🔥
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+// This file initializes/configures Backend with Firebase 🔥
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("firebase-admin/app");
-const serviceAccountKey_json_1 = __importDefault(require("./firebase/serviceAccountKey.json"));
+const serviceAccount = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: (_a = process.env.FIREBASE_PRIVATE_KEY) === null || _a === void 0 ? void 0 : _a.replace(/\\n/g, "\n"),
+};
 const app = (0, app_1.getApps)().length > 0
     ? (0, app_1.getApps)()[0]
     : (0, app_1.initializeApp)({
-        credential: (0, app_1.cert)(serviceAccountKey_json_1.default),
+        credential: (0, app_1.cert)(serviceAccount),
     });
 exports.default = app;
