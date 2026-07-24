89d82c309a15a7d2f3e9a54dff47985cda245dc3
Muskan-codebase <muskanmjvr@gmail.com>
2026-07-24 11:54:42 +0530

    code fix: updated chat validation & chat controller logic

--- backend/src/app/modules/profile-details/chat/chat.validation.ts
+++ backend/src/app/modules/profile-details/chat/chat.validation.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
 export const createChatRoomValidation = z.object({
-    senderId: z.string().trim().min(1),
     receiverId: z.string().trim().min(1),
     interestId: z.string().trim().min(1),
 });
