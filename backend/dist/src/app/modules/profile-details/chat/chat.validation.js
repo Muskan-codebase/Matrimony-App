30a22a71e3640a54a5e4006687e122adb571dc9d
Muskan-codebase <muskanmjvr@gmail.com>
2026-07-25 13:05:41 +0530

    updated the dist folder with latest changes

--- backend/dist/src/app/modules/profile-details/chat/chat.validation.js
+++ backend/dist/src/app/modules/profile-details/chat/chat.validation.js
@@ -3,6 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.createChatRoomValidation = void 0;
 const zod_1 = require("zod");
 exports.createChatRoomValidation = zod_1.z.object({
-    receiverId: zod_1.z.string().trim().min(1),
     interestId: zod_1.z.string().trim().min(1),
 });
