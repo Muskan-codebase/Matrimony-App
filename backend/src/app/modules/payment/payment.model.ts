f867aef65f9c1e2608e4f8be86e154d14f0ae0f9
Muskan-codebase <muskanmjvr@gmail.com>
2026-07-28 15:01:24 +0530

    added packages payment model schema

---
+++ backend/src/app/modules/payment/payment.model.ts
@@ -0,0 +1,70 @@
+import { Schema, model } from "mongoose";
+import { IPayment, PaymentStatus } from "./payment.interface";
+
+
+const paymentSchema = new Schema<IPayment>(
+    {
+
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: "Auth",
+            required: true,
+        },
+
+
+        profileId: {
+            type: Schema.Types.ObjectId,
+            ref: "Profile",
+            required: true,
+        },
+
+
+        packageId: {
+            type: Schema.Types.ObjectId,
+            ref: "Package",
+            required: true,
+        },
+
+
+        amount: {
+            type: Number,
+            required: true,
+        },
+
+
+        razorpayOrderId: {
+            type: String,
+        },
+
+
+        razorpayPaymentId: {
+            type: String,
+        },
+
+
+        razorpaySignature: {
+            type: String,
+        },
+
+
+        status: {
+            type: String,
+            enum: Object.values(PaymentStatus),
+            default: PaymentStatus.PENDING,
+        },
+
+
+        paidAt: {
+            type: Date,
+        }
+
+    },
+    {
+        timestamps: true
+    });
+
+
+export const Payment = model<IPayment>(
+    "Payment",
+    paymentSchema
+);
