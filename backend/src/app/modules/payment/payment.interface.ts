248576e5c650cc7b7bc2e3ddeb7213d68fa31872
Muskan-codebase <muskanmjvr@gmail.com>
2026-07-28 15:01:00 +0530

    added packages payment interface

---
+++ backend/src/app/modules/payment/payment.interface.ts
@@ -0,0 +1,35 @@
+import { Types } from "mongoose";
+
+
+export enum PaymentStatus {
+    PENDING = "PENDING",
+    SUCCESS = "SUCCESS",
+    FAILED = "FAILED",
+}
+
+
+export interface IPayment {
+
+    userId: Types.ObjectId;
+
+    profileId: Types.ObjectId;
+
+    packageId: Types.ObjectId;
+
+
+    amount: number;
+
+
+    razorpayOrderId?: string;
+
+    razorpayPaymentId?: string;
+
+    razorpaySignature?: string;
+
+
+    status: PaymentStatus;
+
+
+    paidAt?: Date;
+
+}
