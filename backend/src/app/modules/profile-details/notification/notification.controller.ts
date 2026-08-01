cf9d17915c181f261cd105ee731d2f9860c08761
Muskan-codebase <muskanmjvr@gmail.com>
2026-08-01 13:53:06 +0530

    added notification controller logic to store logged-in users fcm token

---
+++ backend/src/app/modules/profile-details/notification/notification.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { ZodError } from "zod";
+import * as notificationService from "./notification.service.js";
+import { registerTokenSchema } from "./notification.validation.js";
+
+export const registerToken = async (req: Request, res: Response) => {
+    try {
+        // Validate request body
+        const { token } = registerTokenSchema.parse(req.body);
+
+        await notificationService.registerToken(
+            req.user._id,
+            token
+        );
+
+        return res.status(200).json({
+            success: true,
+            message: "FCM token registered successfully.",
+        });
+
+    } catch (error: any) {
+
+        if (error instanceof ZodError) {
+            return res.status(400).json({
+                success: false,
+                message: error.issues[0].message
+            });
+        }
+
+        return res.status(500).json({
+            success: false,
+            message: error.message || "Internal Server Error",
+        });
+    }
+};
