e7ca71b242a0be44282bec4a532636b9f1f36369
Muskan-codebase <muskanmjvr@gmail.com>
2026-08-01 13:55:40 +0530

    added send notifications service

---
+++ backend/src/app/services/sendNotification.service.ts
@@ -0,0 +1,38 @@
+import app from "../config/firebase";
+import { getMessaging } from "firebase-admin/messaging";
+import Auth from "../modules/auth/auth.model.js";
+
+const messaging = getMessaging(app);
+
+interface SendNotificationParams {
+    receiverId: string;
+    title: string;
+    body: string;
+    data?: Record<string, string>;
+}
+
+export const sendNotification = async ({
+    receiverId,
+    title,
+    body,
+    data = {},
+}: SendNotificationParams) => {
+    const user = await Auth.findById(receiverId).select("fcmTokens");
+
+    const tokens = user?.fcmTokens ?? [];
+
+    if (tokens.length === 0) {
+        return;
+    }
+
+    const messages = tokens.map((token) => ({
+    token,
+    notification: {
+        title,
+        body,
+    },
+    data,
+}));
+
+await messaging.sendEach(messages);
+};
