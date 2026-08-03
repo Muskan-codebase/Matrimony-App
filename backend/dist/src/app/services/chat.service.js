24f3949e1621cf2e7e47ed593bf8bf69ef96d4ca
Muskan-codebase <muskanmjvr@gmail.com>
2026-08-03 15:19:57 +0530

    updated the dist folder

--- backend/dist/src/app/services/chat.service.js
+++ backend/dist/src/app/services/chat.service.js
@@ -148,13 +148,16 @@ const sendMessage = (roomId_1, authUserId_1, text_1, ...args_1) => __awaiter(voi
     /**
      * Restrict messages while interest is pending
      */
-    if (interest.status === interest_status_enum_1.InterestStatus.PENDING) {
-        const messageCounts = roomData.messageCounts || {};
-        const senderMessageCount = messageCounts[senderProfileId] || 0;
-        if (senderMessageCount >= MESSAGE_LIMIT) {
-            throw new Error("You have reached the maximum of 4 messages. Wait until the interest request is accepted.");
-        }
-    }
+    // if (interest.status === InterestStatus.PENDING) {
+    //     const messageCounts = roomData.messageCounts || {};
+    //     const senderMessageCount =
+    //         messageCounts[senderProfileId] || 0;
+    //     if (senderMessageCount >= MESSAGE_LIMIT) {
+    //         throw new Error(
+    //             "You have reached the maximum of 4 messages. Wait until the interest request is accepted."
+    //         );
+    //     }
+    // }
     /**
      * Create message
      */
